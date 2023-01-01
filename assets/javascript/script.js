6e61bf409dd1bb24d35d77f04ee76d13f514541e
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-01-01 10:25:21 -0600

    Added time deduction for wrong answers and further comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,8 @@ const header = document.querySelector("#startQuiz");  // header and start button
 const quiz = document.querySelector("#quiz");         // quiz section
 const start = document.querySelector("#startBtn");    // "Start Quiz" button
 
-const startingTime = 90;     // Clock starts at this time
+const startingTime = 90;        // clock starts at this time
+const penalty = 5;              // amount of seconds penalty for wrong answer
 
 
 start.addEventListener("click", function() {
@@ -33,8 +34,8 @@ function takeQuiz() {
     // Function that decrements the main countdown clock
     let countdown = function() {
         timeRemaining--;
-        timer.textContent = convertToTime(timeRemaining);
-        if(timeRemaining==0) {
+        updateClock(timeRemaining);
+        if(timeRemaining<=0) {
             console.log("Time's up!");
             endQuiz();
         }
@@ -43,7 +44,13 @@ function takeQuiz() {
     // Function for when a user answers a question correctly
     let correctAnswer = function(e) {
         e.preventDefault();
+
+        // mark the question as having been answered
+        quizQs[questionNumber].answered = true;
+
+        // record which option was clicked
         quizQs[questionNumber].options[e.target.dataset.nth].clicked=true;
+
         totalRight++;
         console.log("correct!");
         if( (questionNumber + 1) == quizQs.length) {
@@ -59,8 +66,29 @@ function takeQuiz() {
     // Function for when a user answers a question incorrectly
     let wrongAnswer = function(e) {
         e.preventDefault();
+        quizQs[questionNumber].answered = true;
+
+console.log("Was this really wrong?");
+console.log(quizQs[questionNumber].options[e.target.dataset.nth].correct);
+
         quizQs[questionNumber].options[e.target.dataset.nth].clicked=true;
         console.log("wrong!");
+
+        // remove penalty seconds from timer
+        timeRemaining -= penalty;
+        stopClock();    // so clock doesn't get 
+
+        // check if that exhausts the timer
+        if (timeRemaining <=0) {
+            timeRemaining = 0;
+            endQuiz();
+            updateClock(0);
+            return;
+        } else {
+            updateClock(timeRemaining);
+            startClock();
+        }
+
         if( (questionNumber + 1) == quizQs.length) {
             // this was the last question
             endQuiz();
@@ -122,7 +150,7 @@ function takeQuiz() {
     // Function that tidies up after the quiz is over
     function endQuiz(finished) {
         // stop the timer
-        clearInterval(mainTimer);
+        stopClock();
         console.log("All done!");
 
         // inform the user of the results
@@ -138,6 +166,17 @@ function takeQuiz() {
             opts[i].removeEventListener("click", correctAnswer);
             opts[i].removeEventListener("click", wrongAnswer);
         }
+
+        saveResults(quizQs, totalRight, timeRemaining);
+    }
+
+    // Utilities to start and stop the clock to make coe more readable
+    function startClock() {
+        mainTimer = setInterval(countdown, 1000);
+    }
+
+    function stopClock() {
+        clearInterval(mainTimer);
     }
 
     // Utility that takes a total number of seconds and returns a m:ss string
@@ -148,18 +187,40 @@ function takeQuiz() {
         return timeString;
     }
 
+    // Utility that takes an array and shuffles it
     function shuffleMe(arr) {
-        let counter = arr.length;
         let rand, temp;
+        
+        // start a counter at the end of the array
+        let counter = arr.length;
         while (counter > 0) {
-            rand = Math.floor(Math.random() * counter);
+            // decrement counter
             counter--;
+            // randomly pick an index up through the current counter
+            rand = Math.floor(Math.random() * counter);
+            // swap the random-index element with the counter element
             temp = arr[counter];
             arr[counter] = arr[rand];
             arr[rand] = temp;
         }
+
+        // "dummy" return (array was shuffled in place)
+        return false;
+    }
+
+    // Utility that updates the user view of time remaining
+    function updateClock(remaining) {
+        timer.textContent = convertToTime(remaining);
     }
  
     /* END FUNCTION DECLARATIONS */
     
+}
+
+function saveResults(summary, correct, finished) {
+    console.log(summary);
+    console.log("Going to ask to save results");
+
+    // TEST: iterate over summary object and compare it's output
+    // of correct answers vs. the incremented total
 }
